6baba4fc6426afc7ade87ec418a16ecdd8607cb8
shannoncancello <shannon.cancello@gmail.com>
2013-04-16 20:16:27 -0400

    new

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130404231600) do
+ActiveRecord::Schema.define(:version => 20130417001119) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(:version => 20130404231600) do
     t.string   "remember_token"
     t.boolean  "admin",           :default => false
     t.string   "username"
+    t.string   "auth_token"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
